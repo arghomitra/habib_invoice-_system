520d8cb8660feaf4af16681b90c94d9e0b841de5
Argho <117085618+arghomitra@users.noreply.github.com>
2025-12-12 10:12:46 +0100

    update

--- sw.js
+++ sw.js
@@ -1,14 +1,17 @@
+const CACHE_NAME = 'nimo-cache-v1';
+const FILES_TO_CACHE = [
+  '/habib_invoice-_system/',
+  '/habib_invoice-_system/index.html',
+  '/habib_invoice-_system/manifest.json',
+  '/habib_invoice-_system/sw.js',
+  '/habib_invoice-_system/icon-192.png',
+  '/habib_invoice-_system/icon-512.png'
+];
+
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('nimo-cache-v1').then(cache => {
-      return cache.addAll([
-        '/habib_invoice-_system/',
-        '/habib_invoice-_system/index.html',
-        '/habib_invoice-_system/manifest.json',
-        '/habib_invoice-_system/sw.js',
-        '/habib_invoice-_system/image.png',
-        '/habib_invoice-_system/image.png'
-      ]);
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
     })
   );
 });
