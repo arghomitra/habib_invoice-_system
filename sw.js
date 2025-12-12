dcd3e2cadc32c3ca10f9b340ff3677a77f742b19
Argho <117085618+arghomitra@users.noreply.github.com>
2025-12-12 10:04:40 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,10 +2,12 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open('nimo-cache-v1').then(cache => {
       return cache.addAll([
-        '/index.html',
-        '/manifest.json',
-        '/image.png',
-        '/image.png'
+        '/habib_invoice-_system/',
+        '/habib_invoice-_system/index.html',
+        '/habib_invoice-_system/manifest.json',
+        '/habib_invoice-_system/sw.js',
+        '/habib_invoice-_system/image.png',
+        '/habib_invoice-_system/image.png'
       ]);
     })
   );
